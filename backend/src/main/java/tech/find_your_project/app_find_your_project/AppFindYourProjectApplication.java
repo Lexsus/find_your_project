7931e66fed3ab22aebe0d2bb934f330f4c7e607b
Elena Radchenko <elena.radchenko.9@mail.ru>
2024-01-15 17:23:00 +0300

    commit 1 Создание структуры приложения для бэкенда

---
+++ backend/src/main/java/tech/find_your_project/app_find_your_project/AppFindYourProjectApplication.java
@@ -0,0 +1,13 @@
+package tech.find_your_project.app_find_your_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFindYourProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppFindYourProjectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/tech/find_your_project/app_find_your_project/AppFindYourProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.find_your_project.app_find_your_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppFindYourProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
